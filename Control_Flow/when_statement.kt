199983512d7921fb2ea04768edcc045ab64658e3
zhijie.xia <zhijiexiacs@gmail.com>
2023-02-27 00:05:48 -0700

    When

---
+++ Control_Flow/when_statement.kt
@@ -0,0 +1,19 @@
+fun cases(obj: Any) {
+    when(obj) { // this is like switch statement
+        1 -> println("One")
+        "Hello" -> println("Greeting")
+        is Long -> println("Long")
+        !is String -> println("Not a string")
+        else -> println("Unknown")
+    }
+}
+
+fun main() {
+    cases("Hello")
+    cases(1)
+    cases(0L)
+    cases(MyClass())
+    cases("hello")
+}
+
+class MyClass
